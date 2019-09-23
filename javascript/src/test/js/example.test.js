a259bb0ea7c1846089dd2fedf5680cb9bc8c9eef
Christian Dräger <christian.draeger1@gmail.com>
2019-09-23 12:40:01 +0200

    add a basic js example that imports function

--- javascript/src/test/js/example.test.js
+++ javascript/src/test/js/example.test.js
@@ -1,3 +1,5 @@
+const sum = require('../../main/js/example');
+
 describe('es6 tests with jest', () => {
 
     test('example using jest matchers', () => {
@@ -8,6 +10,16 @@ describe('es6 tests with jest', () => {
     test('example using jest-extended matchers', () => {
         const subjectUnderTest = ["Eris", "Thor", "Anubis", "Ra"];
         expect(subjectUnderTest).toIncludeSameMembers(["Ra", "Eris", "Thor", "Anubis"]);
+    });
+
+    test('sum will work with natural numbers', () => {
+        const result = sum(5, 3);
+        expect(result).toBe(8)
+    });
+
+    test('sum will work with negative numbers', () => {
+        const result = sum(5, -3);
+        expect(result).toBe(2)
     });
 
 });
