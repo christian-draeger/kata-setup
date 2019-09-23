72b3cf304c73aced05fbeb7452db2f0b2df72055
Christian Dräger <christian.draeger1@gmail.com>
2019-09-23 13:55:17 +0200

    download node and yarn into root projects build folder

--- javascript/build.gradle.kts
+++ javascript/build.gradle.kts
@@ -2,6 +2,9 @@ node {
     version = "12.7.0"
     yarnVersion = "1.17.3"
     download = true
+    workDir = file("${project.rootDir}/build/nodejs")
+    yarnWorkDir = file("${project.rootDir}/build/yarn")
+    nodeModulesDir = file("${project.rootDir}/build/node_modules")
 }
 
 tasks {
