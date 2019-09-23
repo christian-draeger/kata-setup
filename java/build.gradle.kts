60d6e3b23051ed1cb94b087e1eae4629e790e9f6
Christian Dräger <christian.draeger1@gmail.com>
2019-09-23 10:44:16 +0200

    add explicit junit version for java module

--- java/build.gradle.kts
+++ java/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:+")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.5.2")
     testImplementation("org.assertj:assertj-core:+")
     testImplementation("org.hamcrest:hamcrest:2.1")
 }
