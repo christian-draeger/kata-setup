3f1acc46462e1742d5a56075f8f1518608dbd0e7
Christian Dräger <christian.draeger1@gmail.com>
2019-09-23 10:45:32 +0200

    include required test engines to kotlin module to support kotlintest and spek

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -1,11 +1,16 @@
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:+")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.5.2")
     testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.1")
     testImplementation("org.spekframework.spek2:spek-dsl-jvm:2.0.0")
     testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:2.0.0")
-    // testRuntimeOnly("org.jetbrains.kotlin:kotlin-reflect:1.3.41")
     testImplementation("io.strikt:strikt-core:+")
     testImplementation("io.mockk:mockk:+")
     testImplementation("org.assertj:assertj-core:+")
     testImplementation("com.willowtreeapps.assertk:assertk-jvm:+")
+}
+
+tasks.withType(Test::class.java).configureEach {
+    useJUnitPlatform {
+        includeEngines.addAll(arrayOf("spek2", "junit-jupiter", "kotlintest"))
+    }
 }
