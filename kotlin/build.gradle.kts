5b25280526902810adc5b78bb481164dc8c18d1c
Christian Dräger <christian.draeger1@gmail.com>
2019-09-23 14:11:05 +0200

    bump spek version to make it work in combination with junit5 and kotlinTests from within same module

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -1,8 +1,8 @@
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.5.2")
     testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.1")
-    testImplementation("org.spekframework.spek2:spek-dsl-jvm:2.0.0")
-    testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:2.0.0")
+    testImplementation("org.spekframework.spek2:spek-dsl-jvm:2.0.7")
+    testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:2.0.7")
     testImplementation("io.strikt:strikt-core:+")
     testImplementation("io.mockk:mockk:+")
     testImplementation("org.assertj:assertj-core:+")
